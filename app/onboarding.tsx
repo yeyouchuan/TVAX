c5bc5215d81d6eecbd48cda90368d314c3ef14df
不游 <71683364+mefengl@users.noreply.github.com>
2024-07-19 06:17:22 +0800

    preload assets in onboarding

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -3,6 +3,7 @@ import type { ImageSourcePropType } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Slider from '@react-native-community/slider'
+import { useAssets } from 'expo-asset'
 import { Stack, useRouter } from 'expo-router'
 import { StyleSheet } from 'nativewind'
 import { useRef, useState } from 'react'
@@ -110,6 +111,19 @@ function Onboarding({ getStartedRoute, steps }: OnboardingProps) {
 }
 
 export default function Tab() {
+  const [_assets] = useAssets([
+    /* eslint-disable ts/no-require-imports */
+    require('~/assets/images/primary_bg.png'),
+    require('~/assets/images/secondary_bg.png'),
+    require('~/assets/images/card_bg.png'),
+    require('~/assets/images/onboarding_first.png'),
+    require('~/assets/images/onboarding_second.png'),
+    require('~/assets/images/onboarding_input_container.png'),
+    require('~/assets/images/onboarding_ios_health.png'),
+    require('~/assets/images/onboarding_ios_calendar.png'),
+    /* eslint-enable ts/no-require-imports */
+  ])
+
   const steps = [
     {
       content: 'Time is a precious resource, often equated to money, especially in professional settings. ',
