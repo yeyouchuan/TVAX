69257920af63678e2be76790a95bacafbb953e00
不游 <71683364+mefengl@users.noreply.github.com>
2024-07-19 04:52:36 +0800

    add timeline ticket

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,7 @@
 import { useAssets } from 'expo-asset'
 import { Stack, useRouter } from 'expo-router'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, View } from 'react-native'
 
-import CustomHeader from '~/components/CustomHeader'
 import { Button } from '~/components/ui/button'
 import { Text } from '~/components/ui/text'
 
@@ -10,33 +9,49 @@ export default function Tab() {
   // eslint-disable-next-line ts/no-require-imports
   const [_assets] = useAssets([require('~/assets/images/secondary_bg.png')])
 
-  const router = useRouter()
+  // const router = useRouter()
 
   return (
-    <>
-      <Stack.Screen
-        options={{
-          headerTitle: () => (
-            <CustomHeader />
-          ),
-        }}
-      />
-      <ImageBackground
-        // eslint-disable-next-line ts/no-require-imports
-        source={require('~/assets/images/secondary_bg.png')}
-        style={{
-          height: '100%',
-          width: '100%',
-        }}
+    <ImageBackground
+      // eslint-disable-next-line ts/no-require-imports
+      source={require('~/assets/images/secondary_bg.png')}
+      style={{
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      {/* <Button
+        onPress={() => router.replace('onboarding')}
       >
-        <Button
-          onPress={() => router.replace('onboarding')}
-        >
-          <Text>
-            Go to Onboarding
-          </Text>
-        </Button>
-      </ImageBackground>
-    </>
+        <Text>
+          Go to Onboarding
+        </Text>
+      </Button> */}
+      <View className="my-8 flex flex-col gap-0.5">
+        <View className="flex flex-col">
+          <View
+            className="flex h-28 w-full flex-row items-center justify-between"
+          >
+            <View className="mx-2 flex w-1/12 items-center justify-center">
+              <View className="flex size-5 items-center justify-center rounded-full bg-gray-200">
+                <Text>1</Text>
+              </View>
+            </View>
+            <ImageBackground
+              className="w-11/12"
+              resizeMode="stretch"
+              // eslint-disable-next-line ts/no-require-imports
+              source={require('~/assets/images/timeline_ticket.png')}
+            >
+              <View
+                className="flex-1"
+              >
+              </View>
+            </ImageBackground>
+          </View>
+          <View className="ml-2 w-8 border border-[#E8D6D0]/60" />
+        </View>
+      </View>
+    </ImageBackground>
   )
 }
